9cbc9340100f3e9d39cacdd73608abb37fb2b6a8
Zacosk <62122304+Zacosk@users.noreply.github.com>
2021-04-26 13:20:20 +1000

    Added textures to planets
    * Added texture folder in 'images'.
    * Added textures to all planets, the sun and the moon.
    * Added createTexturedSphere function to be used instead of createSphere when using textures

--- js/god_mode/build.js
+++ js/god_mode/build.js
@@ -20,35 +20,45 @@ function createSphere(radius, hlines, vlines, color) {
     return sphere;
 }
 
+//Create a sphere using variable radius, vertical lines, horizontal lines
+function createTexturedSphere(radius, hlines, vlines, textureName) {
+    var material = new THREE.MeshLambertMaterial();
+    var texture = new THREE.TextureLoader().load(textureName);
+    material.map = texture;
+    var geometry_sphere = new THREE.SphereGeometry(radius, hlines, vlines);
+    var sphere = new THREE.Mesh(geometry_sphere, material);
+    return sphere;
+}
+
 //var cube = createCube(2, 2, 2, 0, 1, 0);
 //var sphere = createSphere(1, 20, 20, 1, 0, 1);
 
 var pluto = createSphere(0.465,8.184,8.184,0x9ca6b7);
 var triton = createSphere(0.526,9.2,9.2,0x828f88);
-var neptune = createSphere(9.7,170,170,0x4b70dd);
+var neptune = createTexturedSphere(9.7,170,170,'images/textures/neptunetexture.jpg');
 var ariel = createSphere(0.224,3.956,3.956,0x8e7c72);
 var umbriel = createSphere(0.227,3.99,3.99,0x7b7b7b);
 var oberon = createSphere(0.296,5.169,5.169,0xc0a99f);
 var titania = createSphere(0.306,5.39,5.39,0xd2c6b9);
-var uranus = createSphere(10,176,176,0x4FD0E7);
+var uranus = createTexturedSphere(10,176,176,'images/textures/uranustexture.jpg');
 var dione = createSphere(0.216,3.8,3.5,0x7d7d7d);
 var iapetus = createSphere(0.284,4.99,4.99,0x493729);
 var rhea = createSphere(0.297,5.227,5.227,0xb8b8b8);
 var titan = createSphere(1.07,17.464,17.464,0xd7c461);
-var saturn = createSphere(22.74,400,400,0xf1b954)
+var saturn = createTexturedSphere(22.74,400,400,'images/textures/saturntexture.jpg')
 var callisto = createSphere(0.945,16.6,16.6,0x7c6d60)
 var ganymede = createSphere(1.03,18.16,18.16,0xb4b1b2)
 var europa = createSphere(0.61,10.77,10.77,0xac966f)
 var io = createSphere(0.7,12.57,12.57,0xcabf55)
-var jupiter = createSphere(27.2,482,482, 0xc99039);
+var jupiter = createTexturedSphere(27.2,482,482, 'images/textures/jupitertexture.jpg');
 var deimos = createSphere(0.002,0.035,0.035,0x2596be);
 var phobos = createSphere(0.004,0.07,0.07,0x2596be);
-var mars = createSphere(1.33,23.43,23.43,0xc1440e);
-var earth = createSphere(2.5,44,44,0x6b93d6);
-var moon = createSphere(0.675,11.88,11.88,0xFEFCD7);
-var venus = createSphere(2.5,44,44,0x8b7d82);
-var mercury = createSphere(1,32,32,0x747b81)
-var sun = createSphere(30,55,55,0xFDB813);
+var mars = createTexturedSphere(1.33,23.43,23.43,'images/textures/marstexture.jpg');
+var earth = createTexturedSphere(2.5,44,44,'images/textures/earthtexture.jpg');
+var moon = createTexturedSphere(0.675,11.88,11.88,'images/textures/moontexture.jpg');
+var venus = createTexturedSphere(2.5,44,44,'images/textures/venustexture.jpg');
+var mercury = createTexturedSphere(1,32,32,'images/textures/mercurytexture.jpg')
+var sun = createTexturedSphere(30,55,55,'images/textures/suntexture.jpg');
 
 //asteroids
 var n = 300;
